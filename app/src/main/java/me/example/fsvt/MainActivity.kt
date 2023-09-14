d3e62a544fae98f66be2d268e8b8e7c28fdfd0e4
Ian Fleming <ianfleming678@gmail.com>
2023-09-13 23:53:53 -0700

    Succesfully added B.T. foundation code that works

--- app/src/main/java/me/example/fsvt/MainActivity.kt
+++ app/src/main/java/me/example/fsvt/MainActivity.kt
@@ -3,61 +3,40 @@ package me.example.fsvt
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
-import android.bluetooth.le.BluetoothLeScanner
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign.Companion
-import androidx.compose.ui.tooling.preview.Preview
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import me.example.fsvt.ui.theme.FVSTAppTheme
-import java.util.logging.Logger
+import kotlin.text.StringBuilder
 
 class MainActivity : ComponentActivity() {
 
     private val logTag = "IAN"
 
+    // 1) Create Objects
+    private lateinit var B_Start : Button
+    private lateinit var B_Stop : Button
+    private lateinit var B_Show_Devices : Button
+    private lateinit var TV_Devices : TextView
+    private var bondedDevices : Set<BluetoothDevice>? = null
+
+    // 2) Declare Constant of B.T. Class
+    private val REQUEST_ENABLE_BLUETOOTH = 2
     private lateinit var bluetoothAdapter: BluetoothAdapter
-    private lateinit var bluetoothLeScanner: BluetoothLeScanner
-    private var scanning = false
+    private lateinit var enableBluetoothLauncher: ActivityResultLauncher<Intent>
 
-    // Scan for 10 seconds
-    private val SCAN_PERIOD: Long = 10000
-
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        setContent {
-            FVSTAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Test")
-                }
-            }
-        }
-
+        /*
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothAdapter = bluetoothManager.adapter
         bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
@@ -104,22 +83,55 @@ class MainActivity : ComponentActivity() {
             scanning = false
             bluetoothLeScanner.stopScan(scanCallback)
         }
-    }
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = name,
-        modifier = modifier,
-        textAlign = Companion.Center,
-        fontFamily = FontFamily.Monospace
-    )
-}
+    }*/
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FVSTAppTheme {
-        Greeting("Android")
+        // 3) Fetch Object References
+        B_Start = findViewById(R.id.BT_ON)
+        B_Stop = findViewById(R.id.BT_OFF)
+        B_Show_Devices = findViewById(R.id.BT_SHOW_LIST)
+        TV_Devices = findViewById(R.id.BT_DEVICE_LIST)
+
+        // 4) Create BluetoothAdapter class
+        bluetoothAdapter = (getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+
+        // 5) Check if adapter is null? -> Device supports Bluetooth?
+
+        // 6) Enable Bluetooth
+
+        // 6.1) Register Activity
+        enableBluetoothLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if( result.resultCode == Activity.RESULT_OK ) {
+                // User enabled Bluetooth
+            } else {
+                // The user did not enable Bluetooth
+            }
+        }
+
+        B_Start.setOnClickListener{ _ ->
+            if( !bluetoothAdapter.isEnabled ) {
+                val enableBTIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                enableBluetoothLauncher.launch(enableBTIntent)
+            }
+        }
+
+        // 7) Disable Bluetooth
+
+        B_Stop.setOnClickListener{ _ ->
+            if( bluetoothAdapter.isEnabled ) {
+                bluetoothAdapter.disable()  // THIS (CAN) WORKS!
+            }
+        }
+
+        // 8) Show list of bonded devices
+        B_Show_Devices.setOnClickListener{ _ ->
+            val sb = StringBuilder()
+            bondedDevices = bluetoothAdapter.bondedDevices
+            for( temp in (bondedDevices as MutableSet<BluetoothDevice>?)!!) {
+                sb.append("\n" + temp.name + "\n")
+            }
+
+            TV_Devices.text = sb.toString()
+        }
+
     }
 }
